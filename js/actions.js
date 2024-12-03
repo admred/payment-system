ae691afc6723e0e4668994de945d7e2e1f089656
Luciano <>
2024-12-02 21:14:40 -0300

    remove resume and formated numbers

--- js/actions.js
+++ js/actions.js
@@ -1,61 +1,35 @@
-//  Remove the 'e' . We are not working with scientific notation.
-document.querySelectorAll ('input[type=number]')
-    .forEach((el)=>el.addEventListener("keypress",(ev) => {
-        if(ev.which == 46){
-            return
+const delays=[]
+
+function formatNum(val){
+    const valstr=val+''
+    let [integer,decimal]=valstr.split('.')
+
+    if(!decimal){
+        decimal='00'
+    }else if(decimal.length === 1){
+        decimal+='0'
+    }else{
+        decimal=decimal.slice(0,2)
+    }
+
+    let output=''
+    if(integer.length>0){
+        output=integer.slice(-3)
+        for(let i=3;i<integer.length;i+=3){
+            output=integer.slice(-3-i,-i)+'.'+output
         }
-        if (ev.which != 8 && ev.which != 0 && ev.which < 48 || ev.which > 57){
-            ev.preventDefault();
-        }
-    }));
-// compute all parameters
-document.forms[0].addEventListener("submit",(ev)=>{
-    ev.preventDefault()
-    const base=document.getElementById('base')
-    const inflation=document.getElementById('inflation')
-    const months=document.getElementById('months')
-    const taxes=document.getElementById('taxes')
-
-    if( !base || !inflation || !months){
-        console.error("Bad parameters")
-        return;
     }
-
-    let _base=parseFloat(base.value)
-    _base=_base<0?0:_base
-
-    let _inflation=parseFloat(inflation.value)
-    _inflation=_inflation<0?0:_inflation
-    _inflation/=100
-
-    let  _months=parseInt(months.value)
-    _months=_months<0?0:_months
-
-    let _taxes=0
-    if( taxes && taxes.value ){
-         _taxes=parseFloat(taxes.value)
-    }
-
-    _taxes=_taxes<0?0:_taxes
-
-    draw_resume("#resume",_base,_inflation,_months,_taxes)
-
-
-    draw_evolution("#evolution",_base,_inflation,_months,_taxes)
-
-    const elems=document.querySelectorAll(".hide")
-    if(elems){
-        elems.forEach((el)=>show(el))
-    }
-});
-
+    output+=','+decimal
+    return output
+}
+// show-hide block functions
 function show(el){
     el && el.setAttribute('class','show')
 }
 function hide(el){
     el && el.setAttribute('class','hide')
 }
-
+// update html elements
 function write_field(class_tag,val){
     const els=document.querySelectorAll(class_tag)
     if( !els ){
@@ -65,6 +39,7 @@ function write_field(class_tag,val){
     els.forEach((el)=>el.textContent=val)
 }
 
+// reset all
 function reset_fields(classes){
     write_field(classes,"")
     const el=document.querySelector('#evolution')
@@ -78,11 +53,9 @@ function reset_fields(classes){
     }
 }
 
-document.getElementById('reset').addEventListener('click',()=>{
-    reset_fields(".field-result, .field-result-acc, .field-base, .field-base-acc, .field-comp, .field-comp-acc, .field-taxes, .field-taxes-acc")
-});
 
-function fancy_table(cssid,rows,colnames,footrow){
+// draw a fancy html table
+function dynamic_table(cssid,rows,colnames,footrow){
 
     if( !cssid || !rows || !Array.isArray(rows) || rows.length < 1 ||
         !colnames || !Array.isArray(colnames) || colnames.length < 1 ||
@@ -125,24 +98,49 @@ function add_late(ev){
         return;
     }
 
+
     late_container.innerHTML+=`
-            <input type="text" value="${late_id}" data-target="${late_id}"   readonly>
-            <input type="text" value="${late_count}" data-target="${late_id}" readonly>
-            <input type="button" id="#late-remove"  value="&times;" data-target="${late_id}" onclick="remove_late(${late_id})"  class="button error">`
+        <input type="text" value="${late_id}" data-target="${late_id}"   readonly>
+        <input type="text" value="${late_count}" data-target="${late_id}" readonly>
+        <input type="button" id="#late-remove"  value="&times;" data-target="${late_id}" onclick="remove_late(${late_id})"  class="button error">`
+
 
     late_id.value=""
     late_count.value=""
 
     // re-bind events
+
+    delays.push({late_id,late_count})
+
     document.querySelector('#late-add').addEventListener('click',add_late)
 }
+
+// handling payment late functions
 function remove_late(id){
-    const all=document.querySelectorAll(`[data-target="${id}"]`)
-    if(!all){
-        console.warn("element not found")
+    if(!id){
+        console.error("bad parameter")
         return;
     }
+    id=parseInt(id)
+    const all=document.querySelectorAll(`[data-target="${id}"]`)
+
+    if(!all){
+        console.error("element not found")
+        return;
+    }
+    // upadate html
+
     all.forEach((el)=>el.parentNode.removeChild(el))
+
+
+    // update state
+
+    for(let i=0;i<delays.length;i++){
+        if(delays[i] && delays[i].late_id === id){
+            delete delays[i]
+            break;
+        }
+    }
 }
 
 function is_delayed(month_id){
@@ -156,10 +154,6 @@ function get_delay(month_id){
     return 0
 }
 
-document.querySelector('#late-add').addEventListener('click',add_late)
-
-
-
 function draw_evolution(cssid,base,inflation,months,taxes){
 
     const rows=[]
@@ -168,62 +162,122 @@ function draw_evolution(cssid,base,inflation,months,taxes){
     let comp_ac=0
     let sum_inc=0
     let sum_plain=0
-    const avg=get_average_amount(base,months,inflation,taxes)
+    const avg=get_average_amount_taxed(base,inflation,months,taxes)
     for(let i=0;i<months;i++){
         const comp=base*i*inflation
         const month=i+1
-        const increment=Math.trunc(base+comp+taxes)
+        const increment=base+comp+taxes
         rows.push([
             month,
-            `${Math.trunc(inflation*i*100)}%`,
-            `\$${comp}`,
-            `\$${increment}`,
-            `\$${avg}`
+            `${formatNum(inflation*i*100)}%`,
+            `\$${formatNum(comp)}`,
+            `\$${formatNum(increment)}`,
+            `\$${formatNum(avg)}`
             ])
         comp_ac+=comp+taxes
         sum_inc+=comp+base+taxes
     }
 
     sum_plain=avg*months
-    const footrow=['Total','','',`\$${sum_inc}`,`\$${sum_plain}`]
-    fancy_table(cssid,rows,colnames,footrow)
+    const footrow=['Total','','',`\$${formatNum(sum_inc)}`,`\$${formatNum(sum_plain)}`]
+    dynamic_table(cssid,rows,colnames,footrow)
 }
 
-function get_average_amount(base,months,inflation,tax){
-    // TODO: gauss sum
-    let comp_acc=0
-    if(!tax) tax=0
-    for(let i=0; i<months; i++){
-        comp_acc+=base*inflation*i+tax
+function get_average_amount_taxed(base,inflation,months,tax){
+    // Gauss Sum
+    return (months-1)/2*inflation*base+base+tax
+}
+
+function generate_dataset(base,inflation,months,tax,delays){
+    // TODO: generators
+    const ds=[]
+    const avg=get_average_amount_taxed(base,inflation,months,tax)
+
+    // TODO: delays
+    for(let i=0;i<months;i++){
+        ds.push({
+            month:i+1,
+            infp:formatNum(i*inflation*100),
+            infv:formatNum(i*inflation*base),
+            increment:formatNum(i*inflation*base+base),
+            plain:avg,
+        })
     }
-    return comp_acc/months+base
+    return ds
 }
-function get_average_amount1(base,inflation,months){
-    // TODO: gauss sum
-    let comp_acc=0
-    for(let i=0; i<months; i++){
-        comp_acc+=base*inflation*i
+//
+function* dataset_generator(base,inflation,months,tax,delays){
+    const avg=get_average_amount_taxed(base,inflation,months,tax)
+    //console.log("avg = ",avg)
+    // TODO: delays
+    for(let i=0;i<months;i++){
+        yield {
+            month:i+1,
+            infp:formatNum(i*inflation*100),
+            infv:formatNum(i*inflation*base),
+            increment:formatNum(i*inflation*base+base),
+            plain:avg,
+        }
     }
-    return comp_acc/months
 }
 
-function draw_resume(cssid,base,inflation,months,tax){
-    write_field(".field-base",base)
-    write_field('.field-base-acc',base*months)
+//  fix the number input
+document.querySelectorAll ('input[type=number]')
+    .forEach((el)=>el.addEventListener("keypress",(ev) => {
+        if(ev.which == 46){
+            return
+        }
+        if (ev.which != 8 && ev.which != 0 && ev.which < 48 || ev.which > 57){
+            ev.preventDefault();
+        }
+    }));
 
-    let _comp_avg=get_average_amount1(base,inflation,months)
+document.getElementById('reset').addEventListener('click',()=>{
+    reset_fields(".field-result, .field-result-acc, .field-base, .field-base-acc, .field-comp, .field-comp-acc, .field-taxes, .field-taxes-acc")
+});
 
-    write_field('.field-comp',_comp_avg)
-    write_field('.field-comp-acc',_comp_avg*months)
+document.querySelector('#late-add').addEventListener('click',add_late)
 
-    const _result=Math.trunc(base+_comp_avg+tax)
-    write_field('.field-result',_result)
-    write_field('.field-result-acc',_result*months)
+// main
+document.forms[0].addEventListener("submit",(ev)=>{
+    ev.preventDefault()
+    const base=document.getElementById('base')
+    const inflation=document.getElementById('inflation')
+    const months=document.getElementById('months')
+    const taxes=document.getElementById('taxes')
 
-    const _result_no_tax=Math.trunc(_comp_avg+base)
-    write_field('.field-result-no-tax',_result_no_tax)
-    write_field('.field-result-no-tax-acc',_result_no_tax*months)
 
-    write_field('.field-taxes',tax)
-    write_field('.field-taxes-acc',tax*months)
-}
+    if( !base || !inflation || !months){
+        console.error("Bad parameters")
+        return;
+    }
+
+    let _base=parseFloat(base.value)
+    _base=_base<0?0:_base
+
+    let _inflation=parseFloat(inflation.value)
+    _inflation=_inflation<0?0:_inflation
+    _inflation/=100
+
+    let  _months=parseInt(months.value)
+    _months=_months<0?0:_months
+
+    let _taxes=0
+    if( taxes && taxes.value ){
+         _taxes=parseFloat(taxes.value)
+    }
+    _taxes=_taxes<0?0:_taxes
+
+    draw_evolution("#evolution",_base,_inflation,_months,_taxes)
+
+    const elems=document.querySelectorAll(".hide")
+    if(elems){
+        elems.forEach((el)=>show(el))
+    }
+    //const ds=generate_dataset(_base,_inflation,_months,_taxes,delays)
+    for(let row of dataset_generator(_base,_inflation,_months,_taxes,delays)) {
+        console.log(row)
+    }
+    const result=get_average_amount_taxed(_base,_inflation,_months,_taxes)
+    write_field('.field-result',formatNum(result))
+});
